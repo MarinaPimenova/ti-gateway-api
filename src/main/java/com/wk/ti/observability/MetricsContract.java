670102f1882ef75ba5219236b94618bf282ba1ea
MarinaPimenova <mnfpma@gmail.com>
2026-08-24 11:48:54 +0300

    TI-14: AI Configuration, dependencies alignment

--- src/main/java/com/wk/ti/observability/MetricsContract.java
+++ src/main/java/com/wk/ti/observability/MetricsContract.java
@@ -2,5 +2,5 @@ package com.wk.ti.observability;
 
 public class MetricsContract {
     public static final String METRIC_FAILED_REQUESTS_COUNT = "ti.requests.failed";
-    public static final String METRIC_DURATION_REQUEST_COUNT = "ti.request.duration";
+    public static final String METRIC_REQUEST_DURATION = "ti.request.duration";
 }
